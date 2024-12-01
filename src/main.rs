99a5949625870f8605ca7c6ab281d93fcd5880e5
ssuchichen <ssuchichen@163.com>
2024-12-01 12:26:38 +0800

    ssl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use axum::http::Method;
 use axum::Router;
+use axum_server::tls_rustls::RustlsConfig;
 use dotenv::dotenv;
+use std::env;
+use std::net::SocketAddr;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::{ServeDir, ServeFile};
 use tracing_subscriber::filter::LevelFilter;
@@ -9,8 +12,15 @@ use tracing_subscriber::filter::LevelFilter;
 async fn main() {
     dotenv().ok().unwrap();
     tracing_subscriber::fmt()
-        .with_max_level(LevelFilter::DEBUG)
+        .with_max_level(LevelFilter::INFO)
         .init();
+
+    // ssl
+    let cert_file = env::var("SSL_CERT_FILE").expect("CERT_PATH must be set");
+    let key_file = env::var("SSL_KEY_FILE").expect("CERT_KEY_PATH must be set");
+    let tls_config = RustlsConfig::from_pem_file(cert_file, key_file)
+        .await
+        .unwrap();
 
     let cors =
         CorsLayer::new()
@@ -19,8 +29,12 @@ async fn main() {
 
     let router = Router::new().layer(cors).fallback_service(routes_static());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
-    axum::serve(listener, router).await.unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0], 443));
+
+    axum_server::bind_rustls(addr, tls_config)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
 }
 
 fn routes_static() -> Router {
